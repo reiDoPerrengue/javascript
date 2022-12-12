587cca1565694f2ef411d41edfd77ad48dbeff6c
JM Heidrun <jmdev@universoheidrun.com>
2022-12-12 15:46:46 -0300

    if else

---
+++ aula011/ex008.js
@@ -0,0 +1,7 @@
+var vel = 50
+
+console.log(`A velocidade do seu veículo é ${vel}Km/h`)
+if (vel > 60) {
+    console.log(`Você ultrapassou o limite de velocidade. MULTADO!`)
+}
+console.log (`Dirija sempre com segurança!`)
