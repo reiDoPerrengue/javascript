7ecc6ec8a75dabc23af6c22a9b8f899b053a8786
JM Heidrun <jmdev@universoheidrun.com>
2022-12-15 09:11:38 -0300

    tabuada pronta

--- aula016/ambiente2.js
+++ aula016/ambiente2.js
@@ -1,25 +1,12 @@
 //Estrutura de repetição com teste lógico no fim
 
-var c = 2
-if (c == 1) {
+var vtab = 8 //valor da tabuada recebida
+var fator = 1 
+var res = vtab * fator
 do {
-    console.log(`1 X ${c} = ${c}`)
+    console.log(`${vtab} X ${fator} = ${res}`)
+    
+    fator++
+    var res = vtab * fator
+} while (fator<=10)
 
-    c++
-} while (c<=10)
-}
-else if (c == 2) {
-    var expo = 1
-do {
-    console.log(`2 X ${expo} = ${c}`)
-    c = c+2
-    expo++
-} while(c<=20) }
-
-else if(c==3) {
-do {
-    var expo = 1
-    console.log(`3 X ${expo} = ${c}`)
-    c = c+3
-    expo++
-} while(c<=30) }
--- aula018/ex017/script.js
+++ aula018/ex017/script.js
@@ -2,14 +2,17 @@ function verificar() {
     var tnum = window.document.getElementById('txtnum')
     var num = Number(tnum.value)
     var res = window.document.getElementById('res')
-    if(num.length ==0) {
-        window.alert(`[ERRO] - Digite um número válido!`)  
-    } 
-    else if(num == 1){
-            res.innerHTML = (`o valor informado foi: ${num}`)
-        }
-    else if (num == 2){
-    res.innerHTML = (`o valor informado foi: ${num}`)}
-    else if (num == 3){
-    res.innerHTML = (`o valor informado foi: ${num}`)}
-}
+   // if(num.length ==0) {
+    //    window.alert(`[ERRO] - Digite um número válido!`)  
+   // } 
+    //else {
+    var fator = 1 
+    var res = num * fator
+    do {
+        res.innerHTML(`${num} X ${fator} = ${res}`)
+    
+        fator++
+        var res = num * fator
+    } while (fator<=10)
+    }
+//}
