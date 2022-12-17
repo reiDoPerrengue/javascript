1ab1b62bf20924e39c61ddbc2d6747ded436f59c
JM Heidrun <jmdev@universoheidrun.com>
2022-12-17 13:17:58 -0300

    fiz desafio de calculadora de médias

---
+++ aula017/ambiente2.js
@@ -0,0 +1,9 @@
+var nota = [ 6, 10, 5]
+var sum = nota.reduce((accumulator,value) => accumulator + value,0);
+var notafinal = (sum) /nota.length
+
+if(notafinal <=6) {
+    console.log ('Aluno REPROVADO com nota final '+Math.trunc((notafinal)))
+} else{
+    console.log ('Aluno APROVADO com nota final '+Math.trunc((notafinal)))
+}
