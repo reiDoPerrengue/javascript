82cfee8307663443b4e561de1e1623bab79bf812
JM Heidrun <jmdev@universoheidrun.com>
2022-12-19 22:58:05 -0300

    ;

--- aula017/ambiente2.js
+++ aula017/ambiente2.js
@@ -1,6 +1,6 @@
 var nota = [ 6, 10, 5]
 var sum = nota.reduce((accumulator,value) => accumulator + value,0);
-var notafinal = (sum) /nota.length
+var notafinal = (sum) /nota.length;
 
 if(notafinal <=6) {
     console.log ('Aluno REPROVADO com nota final '+Math.trunc((notafinal)))
